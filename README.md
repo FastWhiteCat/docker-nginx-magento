c92593769ba9cd78bb7abca917fd76d4bab4e59a
Adam Hyski <adam.hyski@fastwhitecat.com>
2017-07-24 15:09:47 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+
+# Container with Nginx for Magento 1.x
+
+Included:
+
+- Nginx
+- Files configuration
+
+## Description
+
+Docker container Nginx for Magento 1.x.
+
+## Quick Start
+
+Run:
+
+```bash
+docker run -Pit --name=nginx fastwhitecat/nginx-magento
+```
+Run with configuration:
+
+```bash
+docker run -Pit --name=nginx \
+        -e SERVER_APP="127.0.0.1" \
+        -e SERVER_PORT="9000" \
+        -e MAGE_MODE="developer" \
+        -e MAGE_ROOT="/var/www/html/" fastwhitecat/nginx-magento
+```
+
+Build:
+
+```bash
+
+docker build -t nginx .
+
+```
---
+++ start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -z "$SERVER_APP" ]; then
+    SERVER_APP="app"
+fi
+
+if [ -z "$SERVER_PORT" ]; then
+    SERVER_PORT="9000"
+fi
+
+if [ -z "$MAGE_ROOT" ]; then
+    MAGE_ROOT="/opt/magento"
+fi
+
+if [ -z "$SERVER_HTTPS" ]; then
+    SERVER_HTTPS="off"
+fi
+
+sed -i 's=__server_app__='"$SERVER_APP"'=g' /etc/nginx/conf.d/default.conf
+sed -i 's=__server_port__='"$SERVER_PORT"'=g' /etc/nginx/conf.d/default.conf
+sed -i 's=__mage_root__='"$MAGE_ROOT"'=g' /etc/nginx/conf.d/default.conf
+sed -i 's=__is_http_on__='"$SERVER_HTTPS"'=g' /etc/nginx/conf.d/default.conf
+
+nginx -g 'daemon off;'
